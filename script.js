c17946f6c8c4445cf94764c1ba969f03bd87de08
JobVillagran <jobvillagran@gmail.com>
2023-06-20 22:47:57 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function addIncomeField() {
+    var incomeFields = document.getElementById('incomeFields');
+    var newIncomeField = document.createElement('div');
+    newIncomeField.classList.add('income-field');
+    var newInput = document.createElement('input');
+    newInput.type = 'number';
+    newInput.placeholder = 'Ingreso';
+    newIncomeField.appendChild(newInput);
+    incomeFields.appendChild(newIncomeField);
+  }
+
+  function removeIncomeField() {
+    var incomeFields = document.getElementById('incomeFields');
+    var lastIncomeField = incomeFields.lastElementChild;
+    if (lastIncomeField) {
+      incomeFields.removeChild(lastIncomeField);
+    }
+  }
+
+  function calculateIncome() {
+    var incomeFields = document.querySelectorAll('#incomeFields input[type="number"]');
+    var totalIncome = 0;
+    for (var i = 0; i < incomeFields.length; i++) {
+      var income = parseFloat(incomeFields[i].value);
+      if (!isNaN(income)) {
+        totalIncome += income;
+      }
+    }
+
+    document.getElementById('totalIncome').textContent = 'Ingreso total: ' + totalIncome.toFixed(2);
+  }
